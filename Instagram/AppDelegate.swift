a0a15494ee277294efbc3b34b4dee0429ebf7770
kara-kun <t.kara.kun@gmail.com>
2021-05-17 11:07:41 +0900

    table view cell実装開始

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+//
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         return true
+        
+        
     }
 
     // MARK: UISceneSession Lifecycle
--- Instagram/PostTableViewCell.swift
+++ Instagram/PostTableViewCell.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class PostTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
